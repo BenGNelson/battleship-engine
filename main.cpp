a66af21c4e184ff89c0d0a045ef7b481171f4925
Ben Nelson <bgnelson25@gmail.com>
2021-02-03 14:47:24 -0600

    Added functionality to menu. Users can not 'play' or 'exit.'

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+	// display banner
 	cout << "                   ____   _       ____  __ __                            \n";
 	cout << "                  |    \\ | |     /    ||  |  |                           \n";
 	cout << "                  |  o  )| |    |  o  ||  |  |                           \n";
@@ -19,6 +20,7 @@ int main() {
 	cout << "  |_____||__|__|  |__|    |__|  |_____||_____|  \\___||__|__||____||__|   \n";
 	cout << "                                                                         \n";
 
+	// display menu
 	cout << "Main Menu\n";
 	cout << "1. Play\n";
 	cout << "2. Exit\n";
@@ -26,5 +28,19 @@ int main() {
 	string selection;
 	cin >> selection;		
 	
+	// ensure user makes correct choice
+	while (selection.compare("1") != 0 && selection.compare("2") != 0) {
+		cout << "Please make a proper selection by entering 1 or 2\nEnter your selection: ";
+		cin >> selection;
+	}
+
+	// act on user choice by beginning the game or exiting
+	if (selection.compare("1") == 0) {
+		cout << "Here is the game\n";
+		// play the game
+	} else if (selection.compare("2") == 0) {
+		cout << "Exiting...\n";
+		// exit
+	}
 	return 0;
 }
