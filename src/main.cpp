2a5311156a1e2a14eeb3f79864ced0b76ced52a5
Ben Nelson <bgnelson25@gmail.com>
2021-02-27 17:11:32 -0600

    Updated formatting and comments to main.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@ void start_main_menu();
 
 int main()
 {
+	// set seed for random numbers
+	// where should this go?
 	srand((unsigned)time(0));
+	
 	//TODO Make this start main menu
 	start_main_menu();
 	//Start_game();
